a789ccca50817023695f7592f5496ca17675ffe8
Кургузов Кирилл <kskurguzov@sberbank.ru>
2023-03-16 15:38:54 +0300

    удаляет css файлы

--- webpack.config.js
+++ webpack.config.js
@@ -30,10 +30,6 @@ const config = {
                 exclude: ['/node_modules/'],
             },
             {
-                test: /\.css$/i,
-                use: [stylesHandler, 'css-loader'],
-            },
-            {
                 test: /\.s[ac]ss$/i,
                 use: [stylesHandler, 'css-loader', 'sass-loader'],
             },
